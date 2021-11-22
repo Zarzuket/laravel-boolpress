89ccbad2e35092e964ccbb9c46cf6a6976ecb969
Zarzuket <davide.bajo@gmail.com>
2021-11-22 18:15:09 +0100

    adding vue

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                {{post.title}}
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    mounted(){
+        axios.get('/api/posts')
+        .then((response) => {
+            this.posts = response.data.data;
+        })
+    }
+}
+</script>
---
+++ resources/views/guest/api.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Api?</title>
+</head>
+<body>
+    wewe
+    <div id="app">
+
+    </div>
+
+    <script src="{{asset("js/api.js")}}"></script>
+</body>
+</html>
